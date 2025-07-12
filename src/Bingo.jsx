f5f2dfbe90c81b3135d3d9d05e5896491bb6177c
GeorgeAmakasu <george@freegufo.com>
2025-07-13 01:43:30 +0900

    wip

--- src/Bingo.jsx
+++ src/Bingo.jsx
@@ -94,7 +94,7 @@ console.log(import.meta.env.BASE_URL)
           >
             <div className="slot-image-wrapper">
               <img
-  src={`images/${item.id}`}
+  src={`images/${item.image}`}
   alt={item.name}
   className="slot-image"
 />
